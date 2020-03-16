4fbef6e13657e2ce0987994d7e336074bd736e83
ahmed <ahmedmensh8@gmail.com>
2020-03-16 11:51:09 +0200

    my point

--- app/src/main/java/com/android/pharous/app/lava/ui/point/PointFragment.kt
+++ app/src/main/java/com/android/pharous/app/lava/ui/point/PointFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 import com.android.pharous.app.lava.R
+import kotlinx.android.synthetic.main.fragment_point.*
 
 /**
  * A simple [Fragment] subclass.
@@ -15,4 +19,22 @@ import com.android.pharous.app.lava.R
 class PointFragment : Fragment(R.layout.fragment_point) {
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        newMembershipCL.setOnClickListener {showCreateNewServiceDialog()  }
+        newServiceCL.setOnClickListener { showCreateNewServiceDialog("Create New Service") }
+    }
+
+    private fun showCreateNewServiceDialog(title : String = "Create New Membership") {
+
+        var builder = AlertDialog.Builder(context!!)
+        var view =  activity?.layoutInflater?.inflate(R.layout.dialog_create_service, null)
+        view?.findViewById<TextView>(R.id.titleTV)?.text = title
+        builder.setView(view)
+        var dialog = builder.create()
+
+        view?.findViewById<ImageView>(R.id.cancelImgV)?.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+    }
 }
